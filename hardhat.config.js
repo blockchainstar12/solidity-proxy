92ef5762f574c3ce50a8e7a374a995ad1e19e76d
Shreyans T <shreyans.tatiya@gmail.com>
2024-12-26 19:32:10 +0530

    Add support for Sepolia network and load environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const { ALCHEMY_API_KEY, ETHERSCAN_API_KEY, SEPOLIA_API_KEY } = process.env;
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -8,9 +11,17 @@ module.exports = {
     cache: "./cache",
     artifacts: "./artifacts"
   },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
+  },
   networks: {
     localhost: {
-      url: "http://127.0.0.1:8545"
+      url: "http://127.0.0.1:8545"  // Your local network
+    },
+    sepolia: {
+      url: `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
+      accounts: [`0x${SEPOLIA_API_KEY}`],
+      chainId: 11155111,
     },
     hardhat: {
       // This is the default network when you run `npx hardhat test`
--- scripts/interact.js
+++ scripts/interact.js
@@ -1,16 +1,16 @@
 const hre = require("hardhat");
 
 async function main() {
-    const Contract_Address = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+    const Contract_Address = "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707"; // Contract address after deploying the contract
     const NFTProxy = await hre.ethers.getContractAt("NFTProxy", Contract_Address);
 
-    const tokenId = "321";
-    const tokenURI = "https://example.com/metadata/321";
-    const extension = hre.ethers.toUtf8Bytes("test extension try 3");
+    const tokenId = "999";
+    const tokenURI = "https://example.com/metadata/999";
+    const extension = hre.ethers.toUtf8Bytes("test extension");
     const event = await NFTProxy.requestMint(tokenId, tokenURI, extension);
 
     const tx = await NFTProxy.requestMint(tokenId, tokenURI, extension, {
-        value: hre.ethers.parseEther("0.1")
+        value: hre.ethers.parseEther("0")
     });
 
     console.log("Transaction sent: ", tx.hash);
